42ca3e6d3f4001f88e27c40430be50e638c96d24
uma <uma@tigeen.ch>
2025-03-13 06:08:40 +0530

    theme added

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,23 @@
 import { Stack } from "expo-router";
-
+import { useEffect, useState } from "react";
+import { Appearance } from "react-native";
+import { PaperProvider } from "react-native-paper";
+import { darkTheme, lightTheme } from "@/src/utils/themes";
 export default function RootLayout() {
-  return <Stack />;
+  const [colorScheme, setColorScheme] = useState(Appearance.getColorScheme());
+
+  useEffect(() => {
+    const subscription = Appearance.addChangeListener(({ colorScheme }) => {
+      setColorScheme(colorScheme);
+    });
+
+    return () => subscription.remove();
+  }, []);
+
+  const isDarkMode = colorScheme === "dark";
+  return (
+    <PaperProvider theme={isDarkMode ? darkTheme : lightTheme}>
+      <Stack />
+    </PaperProvider>
+  );
 }
