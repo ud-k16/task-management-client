d6d6943e374a9f86c6eab244cbedcf8cfa15c77e
uma <uma@tigeen.ch>
2025-03-15 22:05:06 +0530

    loder component changes

--- src/common/components/Loader/index.jsx
+++ src/common/components/Loader/index.jsx
@@ -1,13 +1,27 @@
+import { View } from "react-native";
 import { ActivityIndicator, useTheme } from "react-native-paper";
 
 const Loader = ({ isLoading, color, size = "large" }) => {
   const themes = useTheme();
-  return (
-    <ActivityIndicator
-      animating={isLoading ?? false}
-      color={color ? color : themes.colors.primary}
-      size={size}
-    />
-  );
+  if (isLoading)
+    return (
+      <View
+        style={{
+          width: "100%",
+          height: "100%",
+          justifyContent: "center",
+          alignItems: "center",
+          zIndex: 500,
+          position: "absolute",
+          backgroundColor: themes.colors.backdrop,
+        }}
+      >
+        <ActivityIndicator
+          animating={isLoading ?? false}
+          color={color ? color : themes.colors.primary}
+          size={size}
+        />
+      </View>
+    );
 };
 export default Loader;
