c0d45ea870c3320f68e8df5814656130db6b97e2
uma <uma@tigeen.ch>
2025-03-17 01:37:51 +0530

    logut button disapperas on user not logged in

--- src/common/components/Header/index.jsx
+++ src/common/components/Header/index.jsx
@@ -4,10 +4,12 @@ import MaterialCommunityIcons from "@expo/vector-icons/MaterialCommunityIcons";
 import { useState } from "react";
 import { Menu, useTheme } from "react-native-paper";
 import useLogout from "@/src/auth/hooks/useLogout";
+import { useAuthContext } from "@/src/auth/context/useAuthContext";
 
 const Header = ({ title = "Task Management" }) => {
   const [modalVisible, setModalVisible] = useState(false);
   const { logoutUser } = useLogout();
+  const { authenticated } = useAuthContext();
   const Themes = useTheme();
   const showModal = () => setModalVisible(true);
   const hideModal = () => setModalVisible(false);
@@ -24,35 +26,37 @@ const Header = ({ title = "Task Management" }) => {
     titleTextStyle: {
       fontWeight: 600,
       fontSize: moderateScale(26),
-      flex: 0.8,
+      flex: authenticated ? 0.8 : 0.9,
       color: Themes.colors.onSecondary,
     },
   });
   return (
     <View style={styles.container}>
       <Text style={styles.titleTextStyle}>{title}</Text>
-      <View>
-        <Menu
-          visible={modalVisible}
-          onDismiss={hideModal}
-          anchor={
-            <MaterialCommunityIcons
-              name="menu"
-              size={24}
-              color={Themes.colors.background}
-              onPress={showModal}
+      {authenticated && (
+        <View>
+          <Menu
+            visible={modalVisible}
+            onDismiss={hideModal}
+            anchor={
+              <MaterialCommunityIcons
+                name="menu"
+                size={24}
+                color={Themes.colors.background}
+                onPress={showModal}
+              />
+            }
+          >
+            <Menu.Item
+              title="Logout"
+              onPress={() => {
+                hideModal();
+                logoutUser();
+              }}
             />
-          }
-        >
-          <Menu.Item
-            title="Logout"
-            onPress={() => {
-              hideModal();
-              logoutUser();
-            }}
-          />
-        </Menu>
-      </View>
+          </Menu>
+        </View>
+      )}
     </View>
   );
 };
